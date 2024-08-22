b30683f8d0bc2e7048a5b9f6f27e27addc8a0fbe
Luthfia Eka Hapsari <116625912+Luthfiaeka@users.noreply.github.com>
2024-08-22 08:50:21 +0700

    foooter

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,36 +1,30 @@
-
 import React from 'react';
 import styles from '../styles/footer.module.css';
-import { Facebook, Instagram, YouTube, Send } from '@mui/icons-material';
+import { Facebook, Instagram, YouTube } from '@mui/icons-material';
 import { FaTelegramPlane } from 'react-icons/fa';
-
 
 const Footer: React.FC = () => {
     return (
         <div className={styles.footer}>
             <div className={styles.footerContainer}>
                 <div className={styles.logoSection}>
-                    
-                <div className={styles.logoo}>
-                    <img src="./images/white_logo.png" alt="Logo" className={styles.logo} />
-                    <span className={styles.logoText}>Berita Kini</span>
+                    <div className={styles.logoo}>
+                        <img src="./images/white_logo.png" alt="Logo" className={styles.logo} />
+                        <span className={styles.logoText}>Berita Kini</span>
                     </div>
-                    
                     <p className={styles.copy}>&copy; 2023 Berita Kini. All Rights Reserved.</p>
-                    <p> Ikuti Kami</p>
+                    <p>Ikuti Kami</p>
                     <div className={styles.socialIcons}>
-            
-            <a href="#" aria-label="YouTube" className={styles.iconWrapper}>
-              <YouTube className={styles.icon} />
-            </a>
-            <a href="#" aria-label="Instagram" className={styles.iconWrapper}>
-              <Instagram className={styles.icon} />
-            </a>
-            <a href="#" aria-label="Facebook" className={styles.iconWrapper}>
-              <Facebook className={styles.icon} />
-            </a>
-            
-          </div>
+                        <a href="#" aria-label="YouTube" className={styles.iconWrapper}>
+                            <YouTube className={styles.icon} />
+                        </a>
+                        <a href="#" aria-label="Instagram" className={styles.iconWrapper}>
+                            <Instagram className={styles.icon} />
+                        </a>
+                        <a href="#" aria-label="Facebook" className={styles.iconWrapper}>
+                            <Facebook className={styles.icon} />
+                        </a>
+                    </div>
                 </div>
 
                 <div className={styles.linksSection}>
@@ -44,7 +38,7 @@ const Footer: React.FC = () => {
                             <li><a href="/internasional">Internasional</a></li>
                         </ul>
                     </div>
-                  
+
                     <div>
                         <h4>Bantuan</h4>
                         <ul>
@@ -55,13 +49,13 @@ const Footer: React.FC = () => {
                     </div>
 
                     <div>
-                        <h4>Berlangganan Berita Terbaru</h4>
+                        <h4 className={styles.redText}>Berlangganan Berita Terbaru</h4>
                         <form className={styles.subscriptionForm}>
-                          <input type="email" placeholder="Masukkan email" className={styles.forminput}/>
+                            <input type="email" placeholder="Masukkan email" className={styles.forminput}/>
                             <button type="submit" className={styles.submitButton}>
-                             <FaTelegramPlane className={styles.iconsend} />
+                                <FaTelegramPlane className={styles.iconsend} />
                             </button>
-                         </form>
+                        </form>
                     </div>
                 </div>
             </div>
